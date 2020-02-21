16d1c6197ece8269b4f4c8dc272e3ce71e2208b4
otwald <hinterlanddev@googlemail.com>
2020-02-21 08:46:20 +0100

    added interceptor class

--- src/app/shared/_services/http-interceptor.service.ts
+++ src/app/shared/_services/http-interceptor.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { HttpEvent, HttpHeaders, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { AuthService } from '../../pages/auth/_services/auth.service';
+import { Router } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+export class AppHttpInterceptorService implements HttpInterceptor {
+    constructor(public auth: AuthService, private router: Router) { }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('interceptor running');
+        //get the token from auth service
+        const authToken = this.auth.getToken();
+        if (authToken) {
+            const authReq = req.clone(
+                { headers: req.headers.set('Authorization', `Bearer ${authToken}`) }
+            );
+            console.log('interceptor running with new header');
+            return next.handle(authReq)
+                .pipe(
+                    tap((event: HttpEvent<any>) => {
+                        if (event instanceof HttpResponse) {
+                            console.log('Tap Function', event);
+                        }
+                    }), (err: any) => {
+                        console.log(err);
+                        if (err instanceof HttpErrorResponse) {
+                            if (err.status === 401) {
+                                localStorage.removeItem('token');
+                                this.router.navigate(['/']);
+                            }
+                        } else {
+                            console.log('interceptor without changes');
+                            return next.handle(req);
+                        }
+                    }
+                )
+        }
+    }
+}
 export class HttpInterceptorService {
 
-  constructor() { }
+    constructor() { }
 }
