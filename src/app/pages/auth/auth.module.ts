18805a875431af56770f414423122b1f9d824890
otwald <hinterlanddev@googlemail.com>
2020-02-21 09:10:32 +0100

    added passwort reset component

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
+import { PwResetComponent } from './pw-reset/pw-reset.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, LogoutComponent],
+  declarations: [LoginComponent, RegisterComponent, LogoutComponent, PwResetComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
