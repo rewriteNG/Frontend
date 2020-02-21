e1f878c2409d5f31293d0760de6889db599a6f64
otwald <hinterlanddev@googlemail.com>
2020-02-21 07:34:20 +0100

    added basic character class , needs later to be expanded

---
+++ src/app/pages/center/character/character.ts
@@ -0,0 +1,13 @@
+export class Character {
+    id: number;
+    user_id: number;
+    name: string;
+    home_village: string;
+    current_location: string;
+    faction: string;
+    age: number;
+    picture: string;
+    rank: string;
+    
+    constructor() { }
+}
