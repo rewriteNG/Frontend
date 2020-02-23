6036375334fc9c8bc773aaa83e20438cefba708d
otwald <hinterlanddev@googlemail.com>
2020-02-23 19:45:02 +0100

    small fixes

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder } from '@angular/forms'
 
 import { AuthService } from './../../auth/_services/auth.service';
 import { User } from './../user';
