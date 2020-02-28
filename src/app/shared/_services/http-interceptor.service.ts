db6d978de8393ed11a1db27ba621254890619c3d
otwald <hinterlanddev@googlemail.com>
2020-02-28 09:19:29 +0100

    rework on interceptor to finaly work

--- src/app/shared/_services/http-interceptor.service.ts
+++ src/app/shared/_services/http-interceptor.service.ts
@@ -1,42 +1,49 @@
 import { Injectable, Injector } from '@angular/core';
 import { HttpEvent, HttpHeaders, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { AuthService } from '../../pages/auth/_services/auth.service';
 import { Router } from '@angular/router';
 
 @Injectable()
 export class AppHttpInterceptorService implements HttpInterceptor {
-    constructor(public auth: AuthService, private router: Router) { }
+
+    constructor(
+        public auth: AuthService,
+        private router: Router
+    ) { }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('interceptor running');
+        //checks if header were set and adds them if needed
+        if (!req.headers.has('Content-Type')) {
+            req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
+        }
+        req = req.clone({ headers: req.headers.set('Accept', 'application/json') });
         //get the token from auth service
         const authToken = this.auth.getToken();
-        if (authToken) {
-            const authReq = req.clone(
-                { headers: req.headers.set('Authorization', `Bearer ${authToken}`) }
-            );
-            console.log('interceptor running with new header');
-            return next.handle(authReq)
-                .pipe(
-                    tap((event: HttpEvent<any>) => {
-                        if (event instanceof HttpResponse) {
-                            console.log('Tap Function', event);
-                        }
-                    }), (err: any) => {
-                        console.log(err);
-                        if (err instanceof HttpErrorResponse) {
-                            if (err.status === 401) {
-                                localStorage.removeItem('token');
-                                this.router.navigate(['/']);
-                            }
-                        } else {
-                            console.log('interceptor without changes');
-                            return next.handle(req);
-                        }
-                    }
-                )
+        if (!authToken) {
+            return next.handle(req);
         }
+        const authReq = req.clone(
+            { headers: req.headers.set('Authorization', `Bearer ${authToken}`) }
+        );
+        return next.handle(authReq).pipe(
+            map((event: HttpEvent<any>) => {
+                console.log(event)
+                if (event instanceof HttpResponse) {
+                    console.log('Tap Function', event);
+                }
+                return event;
+            }),
+            catchError((err: HttpErrorResponse) => {
+                if (err.status == 401) {
+                    localStorage.removeItem('token');
+                    this.router.navigate(['/']);
+                }
+                return throwError(err);
+            })
+        );
     }
 }
 export class HttpInterceptorService {
