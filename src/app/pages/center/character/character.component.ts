5520140f896b1d301cb9c2ed6a5bc281481596b2
otwald <hinterlanddev@googlemail.com>
2020-03-21 11:33:29 +0100

    added very basic character overview

--- src/app/pages/center/character/character.component.ts
+++ src/app/pages/center/character/character.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character.component.scss']
 })
 export class CharacterComponent implements OnInit {
-
+  items = [
+    { "key": "Vorname", "value": "Max" },
+    { "key": "Nachname", "value": "Mustermann" },
+    { "key": "Fraktion", "value": "Test Fraktion" },
+    { "key": "Rang", "value": "Noob" },
+    { "key": "Standaort", "value": "Daheim" },
+    { "key": "Beschreibung", "value": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, eligendi nam a porro officia quidem ex magni. Sit placeat incidunt voluptates est aliquam, corporis vitae voluptas u dolores. Est, quae!" }
+  ]
   constructor() { }
 
   ngOnInit() {
