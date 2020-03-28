c4feb73b2fa8655441b1c427a70267b6999ff9fe
otwald <hinterlanddev@googlemail.com>
2020-03-28 08:31:31 +0100

    added state check to update when new char is choosen

--- src/app/pages/center/char-base/char-base.component.ts
+++ src/app/pages/center/char-base/char-base.component.ts
@@ -9,9 +9,24 @@ import { Charbase } from "../charbase";
 })
 export class CharBaseComponent implements OnInit {
   character: Charbase;
+  isCharId: boolean = false;
+  charId: string = "";
   constructor(public char: CharService) {}
 
   ngOnInit(): void {
-    this.char.onGetCharBase().subscribe(resp => (this.character = resp));
+  }
+  ngDoCheck() {
+    if (this.char.isCharChoosen() !== this.isCharId) {
+      this.isCharId = this.char.isCharChoosen();
+      if (this.isCharId === true) {
+        this.char.onGetCharBase().subscribe(resp => (this.character = resp));
+      }
+    }
+    if (this.char.getCharId() !== this.charId) {
+      this.charId = this.char.getCharId();
+      if (this.charId) {
+        this.char.onGetCharBase().subscribe(resp => (this.character = resp));
+      }
+    }
   }
 }
