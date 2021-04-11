b82fb0af4bbe5ca9952f53b540cc5f7a83fb865c
otwald <hinterlanddev@googlemail.com>
2021-04-11 11:33:40 +0200

    added formgroups and validators to input target train value

--- src/app/pages/village/training/training.component.ts
+++ src/app/pages/village/training/training.component.ts
@@ -1,15 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from "@angular/forms";
+import { Charvalue } from "../../center/charvalue";
+import { CharService } from "../../center/_services/char.service";
 
 @Component({
-  selector: 'app-training',
-  templateUrl: './training.component.html',
-  styleUrls: ['./training.component.scss']
+  selector: "app-training",
+  templateUrl: "./training.component.html",
+  styleUrls: ["./training.component.scss"],
 })
 export class TrainingComponent implements OnInit {
+  charavalue: Charvalue = new Charvalue();
+  customValidator(ac, key): ValidationErrors {
+    if (+ac < this.charavalue[key]) {
+      return { tooOld: true };
+    } else {
+      null;
+    }
+  }
+  customStrValidor: ValidatorFn = (ac): ValidationErrors => {
+    return this.customValidator(ac.value, "str");
+  };
+  customDefValidor: ValidatorFn = (ac): ValidationErrors => {
+    return this.customValidator(ac.value, "def");
+  };
 
-  constructor() { }
+  customSpeedValidor: ValidatorFn = (ac): ValidationErrors => {
+    return this.customValidator(ac.value, "Speed");
+  };
+
+  customStaminaValidor: ValidatorFn = (ac): ValidationErrors => {
+    return this.customValidator(ac.value, "Stamina");
+  };
+
+  customChakraValidor: ValidatorFn = (ac): ValidationErrors => {
+    return this.customValidator(ac.value, "Chakra");
+  };
+
+  trainStrForm = this.fb.group({
+    time: [0],
+    value: [0, [this.customStrValidor]],
+  });
+  trainDefForm = this.fb.group({
+    time: [0],
+    value: [0, [this.customDefValidor]],
+  });
+
+  trainSpeedForm = this.fb.group({
+    time: [0],
+    value: [0, [this.customSpeedValidor]],
+  });
+
+  trainStaminaForm = this.fb.group({
+    time: [0],
+    value: [0, [this.customStaminaValidor]],
+  });
+
+  trainChakraForm = this.fb.group({
+    time: [0],
+    value: [0, [this.customChakraValidor]],
+  });
+
+  formHolder = {
+    Stärke: { form: this.trainStrForm, start: "str" },
+    Verteidigung: { form: this.trainDefForm, start: "def" },
+    Geschwindigkeit: { form: this.trainSpeedForm, start: "speed" },
+    Chakra: { form: this.trainChakraForm, start: "chakra" },
+    Ausdauer: { form: this.trainStaminaForm, start: "stamina" },
+  };
+
+  constructor(public fb: FormBuilder, public chara: CharService) {}
 
   ngOnInit(): void {
+    this.chara.onGetCharValue().subscribe((resp) => {
+      this.charavalue = resp;
+    });
   }
 
+  onSubmit(form: FormGroup) {
+    console.log(form.value);
+  }
 }
--- src/app/pages/village/village.module.ts
+++ src/app/pages/village/village.module.ts
@@ -5,10 +5,16 @@ import { VillageRoutingModule } from "./village-routing.module";
 import { RootComponent } from "./root/root.component";
 import { NewsComponent } from "./news/news.component";
 import { VillagenavComponent } from "src/app/layout/villagenav/villagenav.component";
-import { TrainingComponent } from './training/training.component';
+import { TrainingComponent } from "./training/training.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [RootComponent, NewsComponent, VillagenavComponent, TrainingComponent],
-  imports: [CommonModule, VillageRoutingModule],
+  declarations: [
+    RootComponent,
+    NewsComponent,
+    VillagenavComponent,
+    TrainingComponent,
+  ],
+  imports: [CommonModule, VillageRoutingModule, ReactiveFormsModule],
 })
 export class VillageModule {}
