cc166a3df91ff4e99bd4751b7883111b15a899c5
otwald <hinterlanddev@googlemail.com>
2021-05-01 09:03:28 +0200

    rework file format

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
 
-import { User } from '../user';
-import { AuthService } from '../_services/auth.service';
+import { User } from "../user";
+import { AuthService } from "../_services/auth.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
   user: User = new User();
@@ -23,19 +23,24 @@ export class RegisterComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router
   ) {
-    this.regisForm = this.formBuilder.group({
-      "name": ['', [Validators.required, Validators.minLength(4)]],
-      "email": ['', [Validators.required, Validators.email]],
-      "password": ['', [Validators.required, Validators.minLength(8)]],
-      "password_conf": ['']
-    }, { validator: this.checkPass });
+    this.regisForm = this.formBuilder.group(
+      {
+        name: ["", [Validators.required, Validators.minLength(4)]],
+        email: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required, Validators.minLength(8)]],
+        password_conf: [""],
+      },
+      { validator: this.checkPass }
+    );
   }
 
   ngOnInit() {
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
   }
   // convenience getter for easy access to form fields
-  get f() { return this.regisForm.controls; }
+  get f() {
+    return this.regisForm.controls;
+  }
 
   onSubmit(regisForm) {
     this.submitted = true;
@@ -50,11 +55,12 @@ export class RegisterComponent implements OnInit {
       },
       (err) => {
         this.error = err.error;
-      });
+      }
+    );
   }
   checkPass(group: FormGroup) {
-    let origin = group.get('password').value;
-    let copy = group.get('password_conf').value;
+    let origin = group.get("password").value;
+    let copy = group.get("password_conf").value;
     return origin === copy ? null : { notSame: true };
   }
 }
